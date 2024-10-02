a381aa029f71c0f87bbb85c6fba8bc18921293e7
JJSamuel343 <joshuajs92@gmail.com>
2024-10-02 10:21:33 +0800

    aaaa

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HSSE Partners Summit 2024</title>
+</head>
+
+<body>
+  <?php
+  echo "Testing";
+  ?>
+  <script src="/assets/js/index.js"></script>
+</body>
+
+</html>
