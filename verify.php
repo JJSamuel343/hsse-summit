d6ae8ee89a2ac9532f4c621b54e66dbf0fe0da90
JJSamuel343 <joshuajs92@gmail.com>
2024-10-03 08:32:03 +0800

    attendance system done?

--- verify.php
+++ verify.php
@@ -4,11 +4,15 @@ require_once "./server/conn.php";
 
 if (isset($_GET['ticket_number'])) {
 
-  $user = $conn->execute_query("SELECT name, email, designation, company FROM users where ticket_number = ?", [$_GET['ticket_number']])->fetch_assoc();
+  $user = $conn->execute_query("SELECT id, name, email, designation, company FROM users where ticket_number = ?", [$_GET['ticket_number']])->fetch_assoc();
 
   if (!isset($user)) {
     exit();
   }
+
+  $conn->execute_query("INSERT INTO checkin (user_id, `date`) VALUES (?,NOW())", [$user['id']]);
+  $user['id'] = null;
+  unset($user['id']);
 
 
   header("Location: successfulscan.php?user=" . json_encode($user));
